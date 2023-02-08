08061508645322f54880e433ca0583eadd21bab9
Oisin <oc343@cam.ac.uk>
2023-02-08 13:56:41 +0000

    teste

---
+++ test_e.py
@@ -0,0 +1,11 @@
+from floodsystem.stationdata import build_station_list
+from floodsystem.geo import station_by_distance
+from floodsystem.geo import stations_within_radius
+from floodsystem.geo import rivers_with_station
+from floodsystem.geo import stations_by_river
+from floodsystem.geo import rivers_by_station_number
+
+def test_rivers_by_station_number():
+    stations = build_station_list()
+    assert type(rivers_by_station_number(stations,9)) == list
+    assert type(rivers_by_station_number(stations,9)[0]) == tuple
