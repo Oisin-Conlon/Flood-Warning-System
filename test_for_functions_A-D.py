5238e14828f19bf18da03750b8a4d4feaa27399f
Oisin <oc343@cam.ac.uk>
2023-03-02 10:40:22 +0000

    Tests A-D

--- test_for_functions_A-D.py
+++ test_for_functions_A-D.py
@@ -3,6 +3,9 @@ from floodsystem.geo import station_by_distance
 from floodsystem.geo import stations_within_radius
 from floodsystem.geo import rivers_with_station
 from floodsystem.geo import stations_by_river
+from floodsystem.flood import stations_highest_rel_level
+from floodsystem.stationdata import update_water_levels
+from Task2B import stations_level_over_threshold
 
 # all tests for A-D
 
@@ -26,6 +29,18 @@ def test_stations_by_river():
     mtest = stations_by_river(stations)
     ptest = list(mtest.keys())
     assert ptest[0] in rivers_with_station(stations)
+
+def test_stations_highest_rel_level():
+    stations = build_station_list()
+    update_water_levels(stations)
+    assert len(stations_highest_rel_level(stations,10)) == 10
+    
+def test_stations_level_over_threshold():
+    stations = build_station_list()
+    update_water_levels(stations)
+    teststations = stations_level_over_threshold(stations, 0.8)
+    assert teststations[0][1] < 0.8
+
 
 
 
